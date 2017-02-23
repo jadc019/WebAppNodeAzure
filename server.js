0150f1cdf5d4a54fc29fcf4b53a3292a3dbee2f4
JDC <JAdc019@gmail.com>
2017-02-23 16:14:49 -0500

    return path

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const port = process.env.PORT || 80
 const app = express()
 
 // serve static assets normally
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/assets'))
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  //response.sendFile(path.resolve(__dirname, 'assets', 'index.html'))
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('path: ' + path.resolve(__dirname, 'assets', 'index.html'));
 })
 
 app.listen(port)
