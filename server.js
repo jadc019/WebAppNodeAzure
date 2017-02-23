64e267fc672b2a43fb84c1ad6475270fac118091
JDC <JAdc019@gmail.com>
2017-02-23 16:40:40 -0500

    2 routers

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const port = process.env.PORT || 80
 const app = express()
 
 // serve static assets normally
-app.use(express.static(__dirname + '/assets'))
+app.use(express.static(__dirname + '/assets1'))
+app.use(express.static(__dirname + '/assets2'))
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
-app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'assets', 'index.html'))
+app.get('/asset1/*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'assets1', 'index.html'))
+})
+app.get('/asset2/*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'assets2', 'index.html'))
 })
 
 app.listen(port)
