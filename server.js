32997a1d6e4adc0fe31950538b93f87a635ac956
JDC <JAdc019@gmail.com>
2017-02-23 17:12:30 -0500

    MonitorUM y SimuladorUM

--- server.js
+++ server.js
@@ -4,16 +4,27 @@ const port = process.env.PORT || 80
 const app = express()
 
 // serve static assets normally
-app.use(express.static(__dirname + '/assets1'))
-app.use(express.static(__dirname + '/assets2'))
+app.use(express.static(__dirname + '/MonitorUM'))
+app.use(express.static(__dirname + '/SimuladorUM'))
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
-app.get('/asset1/*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'assets1', 'index.html'))
+app.get('/MonitorUM', function (request, response){
+  console.log(' redirect to : ' + path.resolve(__dirname, 'MonitorUM', 'index.html'))
+  response.sendFile(path.resolve(__dirname, 'MonitorUM', 'index.html'))
 })
-app.get('/asset2/*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'assets2', 'index.html'))
+app.get('/MonitorUM/*', function (request, response){
+  console.log(' redirect to : ' + path.resolve(__dirname, 'MonitorUM', 'index.html'))
+  response.sendFile(path.resolve(__dirname, 'MonitorUM', 'index.html'))
+})
+
+app.get('/SimuladorUM', function (request, response){
+  console.log(' redirect to : ' + path.resolve(__dirname, 'SimuladorUM', 'index.html'))
+  response.sendFile(path.resolve(__dirname, 'SimuladorUM', 'index.html'))
+})
+app.get('/SimuladorUM/*', function (request, response){
+  console.log(' redirect to : ' + path.resolve(__dirname, 'SimuladorUM', 'index.html'))
+  response.sendFile(path.resolve(__dirname, 'SimuladorUM', 'index.html'))
 })
 
 app.listen(port)
